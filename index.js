7142ab60aa5daf8fd1ca934c75cb2f3165febf39
Ryan Kramer <rkramer613@gmail.com>
2017-03-09 18:15:41 -0700

    get endpoint added

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+const messages = [];
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const port = 3000;
 
 const app = express();
+    app.use(express.static('assets'))
+    app.use(bodyParser.json())
 
+app.listen(port, function(){
+    console.log(`Listening on port: ${port}`);
+});
+
+app.get('/messages', function(req, res, next){
+    console.log(req.body);
+} )
